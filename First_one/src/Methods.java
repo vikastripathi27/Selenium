dac424184627cd92e44e0d994e46325f29790cda
Vikas ji <Vikas ji@Vikas>
2020-12-18 17:18:40 +0530

    calling from parent class

---
+++ First_one/src/Methods.java
@@ -0,0 +1,10 @@
+
+public class Methods {
+
+	public void ValidateHeader()
+	{
+		System.out.println("I'm into methods page, validated home page");
+
+	}
+
+}
---
+++ First_one/src/Parents.java
@@ -0,0 +1,14 @@
+
+public class Parents {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I have navigated to home page");
+		
+		
+		Methods m=new Methods();
+		m.ValidateHeader();
+
+	}
+
+}
