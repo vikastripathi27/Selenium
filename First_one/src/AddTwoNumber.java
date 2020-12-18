baa0ccd21be8c3a451e79f3f0d448a14567ec09c
Vikas ji <Vikas ji@Vikas>
2020-12-18 14:11:36 +0530

    First Commit

---
+++ First_one/src/AddTwoNumber.java
@@ -0,0 +1,13 @@
+
+public class AddTwoNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		int b=9;
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
